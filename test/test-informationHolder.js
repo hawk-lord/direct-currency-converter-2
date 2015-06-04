017220a74585b421f2893ebc7117f3e19d8dd5b9
Per Johansson <per@johansson.name>
2015-06-04 20:59:26 +0300

    Fix: reset settings Internal change: included code changes from the Google Chrome version.

--- test/test-informationHolder.js
+++ test/test-informationHolder.js
@@ -5,8 +5,10 @@ InformationHolderTest.prototype.test = () => {
     const StorageService = function() {};
     StorageService.prototype.init = () => {
     };
+    const defaultExcludedDomains = ["images.google.com", "docs.google.com", "drive.google.com", "twitter.com"];
+    const defaultEnabledCurrencies = {"SEK":true, "CHF":true, "DKK":true, "EUR":true, "GBP":true, "ISK":true, "JPY":true, "NOK":true, "RUB":true, "USD":true};
     const storageService = new StorageService();
     const iso4217Currencies = [];
-    const informationHolder = new InformationHolder(storageService, null, null, iso4217Currencies, null, null);
+    const informationHolder = new InformationHolder(defaultEnabledCurrencies, defaultExcludedDomains, storageService, null, null, iso4217Currencies, null, null);
 };
 

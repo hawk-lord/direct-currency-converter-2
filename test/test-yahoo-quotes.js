46a92119ee34ff58341e0f8e331144ece298e1fe
Per Johansson <per@johansson.name>
2015-08-28 00:51:33 +0300

    No async needed in test-yahoo-quotes.

--- test/test-yahoo-quotes.js
+++ test/test-yahoo-quotes.js
@@ -1,38 +1,34 @@
-const YahooQuotesServiceProviderTest = AsyncTestCase("YahooQuotesServiceProviderTest");
+const YahooQuotesServiceProviderTest =  TestCase("YahooQuotesServiceProviderTest");
 
 
 
-YahooQuotesServiceProviderTest.prototype.test = (queue) => {
+YahooQuotesServiceProviderTest.prototype.test = () => {
     "use strict";
-    const yahooQuotesServiceProvider = new YahooQuotesServiceProvider(eventAggregator);
+
+
     const MockQuotesService = function() {};
     MockQuotesService.prototype.fetchQuotesFromTo = () => {};
     MockQuotesService.prototype.fetchQuotesToFrom = () => {};
-    const yahooQuotesService = new MockQuotesService();
+    const mockQuotesService = new MockQuotesService();
     const convertFromCurrencies = ["AFN"];
     const convertToCurrency = "EUR";
-    yahooQuotesServiceProvider.loadQuotes(yahooQuotesService, convertFromCurrencies, convertToCurrency);
 
-    var convertFromCurrency;
-    var quote;
-    queue.call("Step 1", function(){
-        console.log("Step 1 log");
-        const onQuoteReceived = function(eventArgs) {
-            convertFromCurrency = eventArgs.convertFromCurrency;
-            quote = eventArgs.quote;
-        };
-        eventAggregator.subscribe("quoteReceived", onQuoteReceived);
-        const response1 = '{ "query": { "count": 1, "created": "2015-05-25T20:39:03Z", "lang": "en-US", "results": { "row": [ { "rate": "0.0151", "symbol": "AFNEUR=X" } ] } } }';
-        yahooQuotesServiceProvider.quotesHandlerFromTo(response1);
-        const response2 = '{ "query": { "count": 1, "created": "2015-05-25T20:39:03Z", "lang": "en-US", "results": { "row": [ { "rate": "66.2251", "symbol": "EURAFN=X" } ] } } }';
-        yahooQuotesServiceProvider.quotesHandlerToFrom(response2);
-    });
+    const yahooQuotesServiceProvider = new YahooQuotesServiceProvider(eventAggregator);
+    yahooQuotesServiceProvider.loadQuotes(mockQuotesService, convertFromCurrencies, convertToCurrency);
 
-    queue.call("Step 2", function(){
-        console.log("Step 2 log");
-        assertEquals("", "AFN", convertFromCurrency);
-        assertEquals("", 0.0151, quote);
-    });
+    var convertFromCurrency = "";
+    var quote = 0;
+    const onQuoteReceived = function(eventArgs) {
+        convertFromCurrency = eventArgs.convertFromCurrency;
+        quote = eventArgs.quote;
+    };
+    eventAggregator.subscribe("quoteReceived", onQuoteReceived);
+    const response1 = '{ "query": { "count": 1, "created": "2015-05-25T20:39:03Z", "lang": "en-US", "results": { "row": [ { "rate": "0.0151", "symbol": "AFNEUR=X" } ] } } }';
+    yahooQuotesServiceProvider.quotesHandlerFromTo(response1);
+    const response2 = '{ "query": { "count": 1, "created": "2015-05-25T20:39:03Z", "lang": "en-US", "results": { "row": [ { "rate": "66.2251", "symbol": "EURAFN=X" } ] } } }';
+    yahooQuotesServiceProvider.quotesHandlerToFrom(response2);
+    assertEquals("", "AFN", convertFromCurrency);
+    assertEquals("", 0.0151, quote);
 
 };
 

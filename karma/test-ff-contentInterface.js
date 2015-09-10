86be971125fb07c6e0e2179a4114eef2d83b6753
Per Johansson <per@johansson.name>
2015-09-10 22:19:11 +0300

    Tests for FirefoxContentInterface.

--- karma/test-ff-contentInterface.js
+++ karma/test-ff-contentInterface.js
@@ -19,9 +19,20 @@ const PageMod = function() {
 
 const tabs = function() {
     "use strict";
-
 };
+tabs.on = function (what, fn) {};
+tabs.open = function () {};
+tabs.activeTab = {};
 
+/*
+ watchForPages: watchForPages,
+ toggleConversion: toggleConversion, conversionEnabled
+ showSettingsTab: showSettingsTab,
+ showTestTab: showTestTab,
+ //registerToTabsEvents: registerToTabsEvents,
+ closeSettingsTab: closeSettingsTab
+
+ */
 
 describe("FirefoxContentInterface", function() {
     "use strict";
@@ -29,6 +40,32 @@ describe("FirefoxContentInterface", function() {
         it("new", function () {
             const informationHolder = new MockInformationHolder();
             const firefoxContentInterface = new FirefoxContentInterface(informationHolder, PageMod, MockContentScriptParams, tabs, eventAggregator);
+        });
+        it("watchForPages", function () {
+            const informationHolder = new MockInformationHolder();
+            const firefoxContentInterface = new FirefoxContentInterface(informationHolder, PageMod, MockContentScriptParams, tabs, eventAggregator);
+            firefoxContentInterface.watchForPages();
+        });
+        it("toggleConversion", function () {
+            const conversionEnabled = true;
+            const informationHolder = new MockInformationHolder();
+            const firefoxContentInterface = new FirefoxContentInterface(informationHolder, PageMod, MockContentScriptParams, tabs, eventAggregator);
+            firefoxContentInterface.toggleConversion(conversionEnabled);
+        });
+        it("showSettingsTab", function () {
+            const informationHolder = new MockInformationHolder();
+            const firefoxContentInterface = new FirefoxContentInterface(informationHolder, PageMod, MockContentScriptParams, tabs, eventAggregator);
+            firefoxContentInterface.showSettingsTab();
+        });
+        it("showTestTab", function () {
+            const informationHolder = new MockInformationHolder();
+            const firefoxContentInterface = new FirefoxContentInterface(informationHolder, PageMod, MockContentScriptParams, tabs, eventAggregator);
+            firefoxContentInterface.showTestTab();
+        });
+        it("closeSettingsTab", function () {
+            const informationHolder = new MockInformationHolder();
+            const firefoxContentInterface = new FirefoxContentInterface(informationHolder, PageMod, MockContentScriptParams, tabs, eventAggregator);
+            firefoxContentInterface.closeSettingsTab();
         });
     });
 });

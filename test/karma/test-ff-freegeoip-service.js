c79f84cb7f56f562d78ef26422cc7baa7689f899
Per Johansson <per@johansson.name>
2015-09-10 22:32:41 +0300

    Tests for FirefoxFreegeoipServiceProvider.

---
+++ test/karma/test-ff-freegeoip-service.js
@@ -0,0 +1,13 @@
+/**
+ * Created by per on 2015-09-10.
+ */
+
+describe("FirefoxFreegeoipServiceProvider", function() {
+    "use strict";
+    describe("#new", function() {
+        it("new", function () {
+            const freegeoipServiceProvider = new FirefoxFreegeoipServiceProvider();
+        });
+    })
+});
+
